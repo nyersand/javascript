8dc7cbe5542954445f55cdd6977646d06428b775
Nyer <Ravannyer40@gmail.com>
2025-09-20 16:52:13 -0300

    Funções em JavaScript

---
+++ aula 16/function01.js
@@ -0,0 +1,9 @@
+function parimpar(n) {
+    if (n%2 == 0) {
+        return "Par"
+    } else {
+        return "Ímpar"
+    }
+}
+let res = parimpar(284573)
+console.log(res)
---
+++ aula 16/function02.js
@@ -0,0 +1,5 @@
+function soma(n1, n2) {
+    return n1 + n2
+}
+let res = soma(543758, 5734987)
+console.log(res)
