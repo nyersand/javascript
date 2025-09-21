89a5a021015242f52cc88188973cda7b0868dbd0
Nyer <Ravannyer40@gmail.com>
2025-09-20 23:38:32 -0300

    Continuação do Exercício de JavaScript

---
+++ aula 16 exerc/ex018/script/script.js
@@ -0,0 +1,59 @@
+let num = document.getElementById("fnum")
+let lista = document.getElementById("lista")
+let res = document.getElementById("res")
+let valores = []
+
+function isNumber(n) {
+    if (Number(n) >= 1 && Number(n) <= 100){
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true 
+    } else {
+        return false
+    }
+}
+function adicionar() {
+   if (isNumero(num.value) && !inLista(num.value, valores)) {
+    valores.push(Number(num.value))
+    let item = document.createElement("option")
+    item.text = `Valor ${num.value} foi adicionado`
+    lista.appendChild(item)
+    res.innerHTML = ""
+} else {
+    alert("Valor inválido ou já encontrado na lista.")
+    }
+    num.value = ""
+    num.focus()
+}
+
+function Finalizar() {
+    if (valores.length == 0) {
+         alert("Adicione valores antes de finalizar")
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for (let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma / tot
+        res.innerHTML = ""
+        res.innerHTML += `Ao todo, temos ${tot} números cadastrados.`
+        res.innerHTML += `O maior valor informado foi ${maior}.`
+        res.innerHTML += `O menor valor informado foi ${menor}.`
+        res.innerHTML += `Somando todos os valores, temos ${soma}.`
+        res.innerHTML += `A média dos valores digitados é ${media}.`
+    } 
+} 
